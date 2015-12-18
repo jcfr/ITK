jcfr/ITK

commit eb2316a3a879579a04ce866931f53dcd2c48d08a

BUG: TransformIO: Implement explicit template instantiation using "extern".
This commit updates explicit instantiation to use "extern" keyword similarly
to what was done in InsightSoftwareConsortium/ITK@b72726c (BUG: Explicitly
instantiate common MetaDataObjects).

By using the recommended approach for template instantiation, this commit
should also fix TransformReadWriteTest where TransformIO failed to be
successfully instantiated. Here is the error that was reported when the
was compiled on MacOSX using clang-6.0:

  libc++abi.dylib: terminating with uncaught exception of type itk::ExceptionObject: /scratch/dashboards/MacOSX-clang6/ITK/Modules/IO/TransformBase/src/itkTransformFileWriterSpecializations.cxx:212:
  itk::ERROR: TransformFileWriterTemplate(0x7fa552100f10): The input of writer should be whether a double precision or a single precision transform type. Called from TransformFileWriterTemplate<double,double$

It also updates IOTransformDCMTK remote module to include a similar fix.

Note #1:

To shut up the following warning from gcc, context specific "pragma GCC
diagnostic" preprocessor instructions have been added:

  warning: type attributes ignored after type is already defined [-Wattributes]

This is most likely specific to gcc (see issue #39159), and at the time
of this commit there were no resolution or information about gcc version
where the "problem" is addressed.
See https://gcc.gnu.org/bugzilla/show_bug.cgi?id=39159

For that reason, the "pragma GCC diagnostic" is applied in-conditionally
of the gcc version being used.

Note #2:

While "extern" is only part of the c++ standard starting with c++11 (see
section 14.7.2 of the standard), it is supported by Visual Studio 2008,
GCC and Clang using compiler specific extension in earlier version:

* Visual Studio 2008: https://msdn.microsoft.com/en-us/library/by56e477(v=vs.90).aspx

* Gcc: https://gcc.gnu.org/onlinedocs/gcc-4.8.4/gcc/Template-Instantiation.html#Template-Instantiation

Modules/Core/Transform/include/itkTransformBase.h
@@ -18,6 +18,8 @@
 #ifndef itkTransformBase_h
 #define itkTransformBase_h
 
+#include "ITKTransformExport.h"
+
 #include "itkObject.h"
 #include "itkPoint.h"
 #include "itkCovariantVector.h"
@@ -146,6 +148,22 @@ private:
 
 /** This helps to meet backward compatibility */
 typedef TransformBaseTemplate< double > TransformBase;
+
+/** Explicit instantiations */
+#ifndef ITK_TEMPLATE_EXPLICIT_TransformBase
+// Explicit instantiation is required to ensure correct dynamic_cast
+// behavior across shared libraries.
+#  if defined( ITKTransform_EXPORTS )
+//   We are building this library
+#    define ITKTransform_EXPORT_EXPLICIT
+#  else
+//   We are using this library
+#    define ITKTransform_EXPORT_EXPLICIT ITKTransform_EXPORT
+#  endif
+extern template class ITKTransform_EXPORT_EXPLICIT TransformBaseTemplate< double >;
+extern template class ITKTransform_EXPORT_EXPLICIT TransformBaseTemplate< float >;
+#  undef ITKTransform_EXPORT_EXPLICIT
+#endif
 
 } // end namespace itk
 

Modules/IO/TransformBase/include/itkCompositeTransformIOHelper.h
@@ -45,7 +45,7 @@ namespace itk
  * \ingroup ITKIOTransformBase
  */
 template<typename TParametersValueType>
-class ITKIOTransformBase_EXPORT CompositeTransformIOHelperTemplate
+class CompositeTransformIOHelperTemplate
 {
 public:
   typedef typename TransformIOBaseTemplate<TParametersValueType>::TransformType          TransformType;
@@ -93,8 +93,24 @@ private:
 /** This helps to meet backward compatibility */
 typedef CompositeTransformIOHelperTemplate<double> CompositeTransformIOHelper;
 
+/** Explicit instantiations */
+#ifndef ITK_TEMPLATE_EXPLICIT_CompositeTransformIOHelper
+// Explicit instantiation is required to ensure correct dynamic_cast
+// behavior across shared libraries.
+#  if defined( ITKIOTransformBase_EXPORTS )
+//   We are building this library
+#    define ITKIOTransformBase_EXPORT_EXPLICIT
+#  else
+//   We are using this library
+#    define ITKIOTransformBase_EXPORT_EXPLICIT ITKIOTransformBase_EXPORT
+#  endif
+extern template class ITKIOTransformBase_EXPORT_EXPLICIT CompositeTransformIOHelperTemplate< double >;
+extern template class ITKIOTransformBase_EXPORT_EXPLICIT CompositeTransformIOHelperTemplate< float >;
+#  undef ITKIOTransformBase_EXPORT_EXPLICIT
+#endif
+
 } // namespace itk
 
-// Note: Explicit instantiation is done in itkTransformFactoryBaseInstantiation.cxx
+// Note: Explicit instantiation is done in itkCompositeTransformIOHelper.cxx
 
 #endif //  itkCompositeTransformIOHelper_h

Modules/IO/TransformBase/include/itkTransformFileReader.h
@@ -100,12 +100,28 @@ private:
 /** This helps to meet backward compatibility */
 typedef itk::TransformFileReaderTemplate<double> TransformFileReader;
 
+/** Explicit instantiations */
+#ifndef ITK_TEMPLATE_EXPLICIT_TransformFileReader
+// Explicit instantiation is required to ensure correct dynamic_cast
+// behavior across shared libraries.
+#  if defined( ITKIOTransformBase_EXPORTS )
+//   We are building this library
+#    define ITKIOTransformBase_EXPORT_EXPLICIT
+#  else
+//   We are using this library
+#    define ITKIOTransformBase_EXPORT_EXPLICIT ITKIOTransformBase_EXPORT
+#  endif
+extern template class ITKIOTransformBase_EXPORT_EXPLICIT TransformFileReaderTemplate< double >;
+extern template class ITKIOTransformBase_EXPORT_EXPLICIT TransformFileReaderTemplate< float >;
+#  undef ITKIOTransformBase_EXPORT_EXPLICIT
+#endif
+
 } // namespace itk
 
 #ifdef ITK_IO_FACTORY_REGISTER_MANAGER
 #include "itkTransformIOFactoryRegisterManager.h"
 #endif
 
-// Note: Explicit instantiation is done in itkTransformFactoryBaseInstantiation.cxx
+// Note: Explicit instantiation is done in itkTransformFileReader.cxx
 
 #endif // itkTransformFileReade_h

commit 0aae40bb0dc356720d9f2c4761b1b327181d3c83

COMP: TransformIO: Fix build errors related to explicit instantiation
This commit fixes a regression introduced in eb2316a (BUG: TransformIO:
Implement explicit template instantiation using "extern".). By moving the
use of custom EXPORT_EXPLICIT macro *OUTSIDE* the header guards, it ensures
the defined and undefined states of ITK_TEMPLATE_EXPLICIT_<classname> macro
are considered within the same compilation unit.

Error fixed by this commit:

  itkTransformIOFactory.h(83): warning C4910: 'itk::TransformIOFactoryTemplate' : '__declspec(dllexport)' and 'extern' are incompatible on an explicit instantiation


For sake of consistency, this commit also simplifies the explicit
instantiation associated with itkMetaDataObject. It wasn't causing
build error because the associated was included only once in the ITKCommon
library (only in itkMetaDataObject.cxx).

Notes:

 * it also updates IOTransformDCMTK remote module applying a
   similar fix.

Change-Id: Ie76720b967d09ebedceecfdd62c6e3a243a1c4b1

Modules/Core/Common/include/itkMetaDataObject.h
@@ -188,41 +188,6 @@ inline bool ExposeMetaData(const MetaDataDictionary & Dictionary, const std::str
   return true;
 }
 
-#ifndef ITK_TEMPLATE_EXPLICIT_MetaDataObject
-// Explicit instantiation is required to ensure correct dynamic_cast
-// behavior across shared libraries.
-#if defined( ITKCommon_EXPORTS )
-// don't use export
-#define ITKCommon_EXPORT_EXPLICIT
-#else
-// only import/hidden
-#define ITKCommon_EXPORT_EXPLICIT ITKCommon_EXPORT
-#endif
-extern template class ITKCommon_EXPORT_EXPLICIT MetaDataObject< bool >;
-extern template class ITKCommon_EXPORT_EXPLICIT MetaDataObject< unsigned char >;
-extern template class ITKCommon_EXPORT_EXPLICIT MetaDataObject< char >;
-extern template class ITKCommon_EXPORT_EXPLICIT MetaDataObject< signed char >;
-extern template class ITKCommon_EXPORT_EXPLICIT MetaDataObject< unsigned short >;
-extern template class ITKCommon_EXPORT_EXPLICIT MetaDataObject< short >;
-extern template class ITKCommon_EXPORT_EXPLICIT MetaDataObject< unsigned int >;
-extern template class ITKCommon_EXPORT_EXPLICIT MetaDataObject< int >;
-extern template class ITKCommon_EXPORT_EXPLICIT MetaDataObject< unsigned long >;
-extern template class ITKCommon_EXPORT_EXPLICIT MetaDataObject< long >;
-extern template class ITKCommon_EXPORT_EXPLICIT MetaDataObject< float >;
-extern template class ITKCommon_EXPORT_EXPLICIT MetaDataObject< double >;
-extern template class ITKCommon_EXPORT_EXPLICIT MetaDataObject< std::string >;
-extern template class ITKCommon_EXPORT_EXPLICIT MetaDataObject< std::vector<float> >;
-extern template class ITKCommon_EXPORT_EXPLICIT MetaDataObject< std::vector<double> >;
-extern template class ITKCommon_EXPORT_EXPLICIT MetaDataObject< std::vector<std::vector<float> > >;
-extern template class ITKCommon_EXPORT_EXPLICIT MetaDataObject< std::vector<std::vector<double> > >;
-extern template class ITKCommon_EXPORT_EXPLICIT MetaDataObject< Array<char> >;
-extern template class ITKCommon_EXPORT_EXPLICIT MetaDataObject< Array<int> >;
-extern template class ITKCommon_EXPORT_EXPLICIT MetaDataObject< Array<float> >;
-extern template class ITKCommon_EXPORT_EXPLICIT MetaDataObject< Array<double> >;
-extern template class ITKCommon_EXPORT_EXPLICIT MetaDataObject< Matrix<double> >;
-#undef ITKCommon_EXPORT_EXPLICIT
-#endif
-
 } // end namespace itk
 
 /**
@@ -280,3 +245,59 @@ extern template class ITKCommon_EXPORT_EXPLICIT MetaDataObject< Matrix<double> >
 #endif
 
 #endif //itkMetaDataObject_h
+
+/** Explicit instantiations */
+#ifndef ITK_TEMPLATE_EXPLICIT_MetaDataObject
+// Explicit instantiation is required to ensure correct dynamic_cast
+// behavior across shared libraries.
+//
+// IMPORTANT: Since within the same compilation unit,
+//            ITK_TEMPLATE_EXPLICIT_<classname> defined and undefined states
+//            need to be considered. This code *MUST* be *OUTSIDE* the header
+//            guards.
+//
+#  if defined( ITKCommon_EXPORTS )
+//   We are building this library
+#    define ITKCommon_EXPORT_EXPLICIT
+#  else
+//   We are using this library
+#    define ITKCommon_EXPORT_EXPLICIT ITKCommon_EXPORT
+#  endif
+namespace itk
+{
+
+#if defined( __GNUC__ )
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wattributes"
+#endif
+
+extern template class ITKCommon_EXPORT_EXPLICIT MetaDataObject< bool >;
+extern template class ITKCommon_EXPORT_EXPLICIT MetaDataObject< unsigned char >;
+extern template class ITKCommon_EXPORT_EXPLICIT MetaDataObject< char >;
+extern template class ITKCommon_EXPORT_EXPLICIT MetaDataObject< signed char >;
+extern template class ITKCommon_EXPORT_EXPLICIT MetaDataObject< unsigned short >;
+extern template class ITKCommon_EXPORT_EXPLICIT MetaDataObject< short >;
+extern template class ITKCommon_EXPORT_EXPLICIT MetaDataObject< unsigned int >;
+extern template class ITKCommon_EXPORT_EXPLICIT MetaDataObject< int >;
+extern template class ITKCommon_EXPORT_EXPLICIT MetaDataObject< unsigned long >;
+extern template class ITKCommon_EXPORT_EXPLICIT MetaDataObject< long >;
+extern template class ITKCommon_EXPORT_EXPLICIT MetaDataObject< float >;
+extern template class ITKCommon_EXPORT_EXPLICIT MetaDataObject< double >;
+extern template class ITKCommon_EXPORT_EXPLICIT MetaDataObject< std::string >;
+extern template class ITKCommon_EXPORT_EXPLICIT MetaDataObject< std::vector<float> >;
+extern template class ITKCommon_EXPORT_EXPLICIT MetaDataObject< std::vector<double> >;
+extern template class ITKCommon_EXPORT_EXPLICIT MetaDataObject< std::vector<std::vector<float> > >;
+extern template class ITKCommon_EXPORT_EXPLICIT MetaDataObject< std::vector<std::vector<double> > >;
+extern template class ITKCommon_EXPORT_EXPLICIT MetaDataObject< Array<char> >;
+extern template class ITKCommon_EXPORT_EXPLICIT MetaDataObject< Array<int> >;
+extern template class ITKCommon_EXPORT_EXPLICIT MetaDataObject< Array<float> >;
+extern template class ITKCommon_EXPORT_EXPLICIT MetaDataObject< Array<double> >;
+extern template class ITKCommon_EXPORT_EXPLICIT MetaDataObject< Matrix<double> >;
+
+#if defined( __GNUC__ )
+#pragma GCC diagnostic pop
+#endif
+
+} // end namespace itk
+#  undef ITKCommon_EXPORT_EXPLICIT
+#endif

Modules/Core/Transform/include/itkTransformBase.h
@@ -149,10 +149,20 @@ private:
 /** This helps to meet backward compatibility */
 typedef TransformBaseTemplate< double > TransformBase;
 
+} // end namespace itk
+
+#endif
+
 /** Explicit instantiations */
 #ifndef ITK_TEMPLATE_EXPLICIT_TransformBase
 // Explicit instantiation is required to ensure correct dynamic_cast
 // behavior across shared libraries.
+//
+// IMPORTANT: Since within the same compilation unit,
+//            ITK_TEMPLATE_EXPLICIT_<classname> defined and undefined states
+//            need to be considered. This code *MUST* be *OUTSIDE* the header
+//            guards.
+//
 #  if defined( ITKTransform_EXPORTS )
 //   We are building this library
 #    define ITKTransform_EXPORT_EXPLICIT
@@ -160,11 +170,21 @@ typedef TransformBaseTemplate< double > TransformBase;
 //   We are using this library
 #    define ITKTransform_EXPORT_EXPLICIT ITKTransform_EXPORT
 #  endif
+namespace itk
+{
+
+#if defined( __GNUC__ )
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wattributes"
+#endif
+
 extern template class ITKTransform_EXPORT_EXPLICIT TransformBaseTemplate< double >;
 extern template class ITKTransform_EXPORT_EXPLICIT TransformBaseTemplate< float >;
-#  undef ITKTransform_EXPORT_EXPLICIT
+
+#if defined( __GNUC__ )
+#pragma GCC diagnostic pop
 #endif
 
 } // end namespace itk
-
+#  undef ITKTransform_EXPORT_EXPLICIT
 #endif

Modules/IO/TransformBase/include/itkCompositeTransformIOHelper.h
@@ -93,10 +93,22 @@ private:
 /** This helps to meet backward compatibility */
 typedef CompositeTransformIOHelperTemplate<double> CompositeTransformIOHelper;
 
+} // namespace itk
+
+// Note: Explicit instantiation is done in itkCompositeTransformIOHelper.cxx
+
+#endif //  itkCompositeTransformIOHelper_h
+
 /** Explicit instantiations */
 #ifndef ITK_TEMPLATE_EXPLICIT_CompositeTransformIOHelper
 // Explicit instantiation is required to ensure correct dynamic_cast
 // behavior across shared libraries.
+//
+// IMPORTANT: Since within the same compilation unit,
+//            ITK_TEMPLATE_EXPLICIT_<classname> defined and undefined states
+//            need to be considered. This code *MUST* be *OUTSIDE* the header
+//            guards.
+//
 #  if defined( ITKIOTransformBase_EXPORTS )
 //   We are building this library
 #    define ITKIOTransformBase_EXPORT_EXPLICIT
@@ -104,13 +116,21 @@ typedef CompositeTransformIOHelperTemplate<double> CompositeTransformIOHelper;
 //   We are using this library
 #    define ITKIOTransformBase_EXPORT_EXPLICIT ITKIOTransformBase_EXPORT
 #  endif
-extern template class ITKIOTransformBase_EXPORT_EXPLICIT CompositeTransformIOHelperTemplate< double >;
-extern template class ITKIOTransformBase_EXPORT_EXPLICIT CompositeTransformIOHelperTemplate< float >;
-#  undef ITKIOTransformBase_EXPORT_EXPLICIT
+namespace itk
+{
+
+#if defined( __GNUC__ )
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wattributes"
 #endif
 
-} // namespace itk
+extern template class ITKIOTransformBase_EXPORT_EXPLICIT CompositeTransformIOHelperTemplate< double >;
+extern template class ITKIOTransformBase_EXPORT_EXPLICIT CompositeTransformIOHelperTemplate< float >;
 
-// Note: Explicit instantiation is done in itkCompositeTransformIOHelper.cxx
+#if defined( __GNUC__ )
+#pragma GCC diagnostic pop
+#endif
 
-#endif //  itkCompositeTransformIOHelper_h
+} // end namespace itk
+#  undef ITKIOTransformBase_EXPORT_EXPLICIT
+#endif

Modules/IO/TransformBase/include/itkTransformFileReader.h
@@ -100,22 +100,6 @@ private:
 /** This helps to meet backward compatibility */
 typedef itk::TransformFileReaderTemplate<double> TransformFileReader;
 
-/** Explicit instantiations */
-#ifndef ITK_TEMPLATE_EXPLICIT_TransformFileReader
-// Explicit instantiation is required to ensure correct dynamic_cast
-// behavior across shared libraries.
-#  if defined( ITKIOTransformBase_EXPORTS )
-//   We are building this library
-#    define ITKIOTransformBase_EXPORT_EXPLICIT
-#  else
-//   We are using this library
-#    define ITKIOTransformBase_EXPORT_EXPLICIT ITKIOTransformBase_EXPORT
-#  endif
-extern template class ITKIOTransformBase_EXPORT_EXPLICIT TransformFileReaderTemplate< double >;
-extern template class ITKIOTransformBase_EXPORT_EXPLICIT TransformFileReaderTemplate< float >;
-#  undef ITKIOTransformBase_EXPORT_EXPLICIT
-#endif
-
 } // namespace itk
 
 #ifdef ITK_IO_FACTORY_REGISTER_MANAGER
@@ -125,3 +109,39 @@ extern template class ITKIOTransformBase_EXPORT_EXPLICIT TransformFileReaderTemp
 // Note: Explicit instantiation is done in itkTransformFileReader.cxx
 
 #endif // itkTransformFileReade_h
+
+/** Explicit instantiations */
+#ifndef ITK_TEMPLATE_EXPLICIT_TransformFileReader
+// Explicit instantiation is required to ensure correct dynamic_cast
+// behavior across shared libraries.
+//
+// IMPORTANT: Since within the same compilation unit,
+//            ITK_TEMPLATE_EXPLICIT_<classname> defined and undefined states
+//            need to be considered. This code *MUST* be *OUTSIDE* the header
+//            guards.
+//
+#  if defined( ITKIOTransformBase_EXPORTS )
+//   We are building this library
+#    define ITKIOTransformBase_EXPORT_EXPLICIT
+#  else
+//   We are using this library
+#    define ITKIOTransformBase_EXPORT_EXPLICIT ITKIOTransformBase_EXPORT
+#  endif
+namespace itk
+{
+
+#if defined( __GNUC__ )
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wattributes"
+#endif
+
+extern template class ITKIOTransformBase_EXPORT_EXPLICIT TransformFileReaderTemplate< double >;
+extern template class ITKIOTransformBase_EXPORT_EXPLICIT TransformFileReaderTemplate< float >;
+
+#if defined( __GNUC__ )
+#pragma GCC diagnostic pop
+#endif
+
+} // end namespace itk
+#  undef ITKIOTransformBase_EXPORT_EXPLICIT
+#endif

Modules/IO/TransformBase/include/itkTransformFileWriter.h
@@ -120,24 +120,6 @@ typedef itk::TransformFileWriterTemplate<double> TransformFileWriter;
 template<> void ITKIOTransformBase_EXPORT TransformFileWriterTemplate< double >::PushBackTransformList(const Object *transObj);
 template<> void ITKIOTransformBase_EXPORT TransformFileWriterTemplate< float >::PushBackTransformList(const Object *transObj);
 
-/** Explicit instantiations */
-#ifndef ITK_TEMPLATE_EXPLICIT_TransformFileWriter
-// Explicit instantiation is required to ensure correct dynamic_cast
-// behavior across shared libraries.
-#  if defined( ITKIOTransformBase_EXPORTS )
-//   We are building this library
-#    define ITKIOTransformBase_EXPORT_EXPLICIT
-#  else
-//   We are using this library
-#    define ITKIOTransformBase_EXPORT_EXPLICIT ITKIOTransformBase_EXPORT
-#  endif
-
-extern template class ITKIOTransformBase_EXPORT_EXPLICIT TransformFileWriterTemplate< double >;
-extern template class ITKIOTransformBase_EXPORT_EXPLICIT TransformFileWriterTemplate< float >;
-
-#  undef ITKIOTransformBase_EXPORT_EXPLICIT
-#endif
-
 #if defined( __GNUC__ )
 #pragma GCC diagnostic push
 #endif
@@ -151,3 +133,39 @@ extern template class ITKIOTransformBase_EXPORT_EXPLICIT TransformFileWriterTemp
 // Note: Explicit instantiation is done in itkTransformFileWriterSpecializations.cxx
 
 #endif // itkTransformFileWriter_h
+
+/** Explicit instantiations */
+#ifndef ITK_TEMPLATE_EXPLICIT_TransformFileWriter
+// Explicit instantiation is required to ensure correct dynamic_cast
+// behavior across shared libraries.
+//
+// IMPORTANT: Since within the same compilation unit,
+//            ITK_TEMPLATE_EXPLICIT_<classname> defined and undefined states
+//            need to be considered. This code *MUST* be *OUTSIDE* the header
+//            guards.
+//
+#  if defined( ITKIOTransformBase_EXPORTS )
+//   We are building this library
+#    define ITKIOTransformBase_EXPORT_EXPLICIT
+#  else
+//   We are using this library
+#    define ITKIOTransformBase_EXPORT_EXPLICIT ITKIOTransformBase_EXPORT
+#  endif
+namespace itk
+{
+
+#if defined( __GNUC__ )
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wattributes"
+#endif
+
+extern template class ITKIOTransformBase_EXPORT_EXPLICIT TransformFileWriterTemplate< double >;
+extern template class ITKIOTransformBase_EXPORT_EXPLICIT TransformFileWriterTemplate< float >;
+
+#if defined( __GNUC__ )
+#pragma GCC diagnostic pop
+#endif
+
+} // end namespace itk
+#  undef ITKIOTransformBase_EXPORT_EXPLICIT
+#endif

Modules/IO/TransformBase/include/itkTransformIOFactory.h
@@ -69,10 +69,22 @@ private:
 /** This helps to meet backward compatibility */
 typedef TransformIOFactoryTemplate<double> TransformIOFactory;
 
+} // end namespace itk
+
+// Note: Explicit instantiation is done in itkTransformIOFactory.cxx
+
+#endif
+
 /** Explicit instantiations */
 #ifndef ITK_TEMPLATE_EXPLICIT_TransformIOFactory
 // Explicit instantiation is required to ensure correct dynamic_cast
 // behavior across shared libraries.
+//
+// IMPORTANT: Since within the same compilation unit,
+//            ITK_TEMPLATE_EXPLICIT_<classname> defined and undefined states
+//            need to be considered. This code *MUST* be *OUTSIDE* the header
+//            guards.
+//
 #  if defined( ITKIOTransformBase_EXPORTS )
 //   We are building this library
 #    define ITKIOTransformBase_EXPORT_EXPLICIT
@@ -80,13 +92,21 @@ typedef TransformIOFactoryTemplate<double> TransformIOFactory;
 //   We are using this library
 #    define ITKIOTransformBase_EXPORT_EXPLICIT ITKIOTransformBase_EXPORT
 #  endif
+namespace itk
+{
+
+#if defined( __GNUC__ )
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wattributes"
+#endif
+
 extern template class ITKIOTransformBase_EXPORT_EXPLICIT TransformIOFactoryTemplate< double >;
 extern template class ITKIOTransformBase_EXPORT_EXPLICIT TransformIOFactoryTemplate< float >;
-#  undef ITKIOTransformBase_EXPORT_EXPLICIT
+
+#if defined( __GNUC__ )
+#pragma GCC diagnostic pop
 #endif
 
 } // end namespace itk
-
-// Note: Explicit instantiation is done in itkTransformIOFactory.cxx
-
+#  undef ITKIOTransformBase_EXPORT_EXPLICIT
 #endif

Modules/IO/TransformHDF5/include/itkHDF5TransformIO.h
@@ -146,10 +146,22 @@ const std::string ITKIOTransformHDF5_EXPORT GetTransformName(int);
 /** This helps to meet backward compatibility */
 typedef HDF5TransformIOTemplate<double> HDF5TransformIO;
 
+} // end namespace itk
+
+// Note: Explicit instantiation is done in itkHDF5TransformIO.cxx
+
+#endif // itkHDF5TransformIO_h
+
 /** Explicit instantiations */
 #ifndef ITK_TEMPLATE_EXPLICIT_HDF5TransformIO
 // Explicit instantiation is required to ensure correct dynamic_cast
 // behavior across shared libraries.
+//
+// IMPORTANT: Since within the same compilation unit,
+//            ITK_TEMPLATE_EXPLICIT_<classname> defined and undefined states
+//            need to be considered. This code *MUST* be *OUTSIDE* the header
+//            guards.
+//
 #  if defined( ITKIOTransformHDF5_EXPORTS )
 //   We are building this library
 #    define ITKIOTransformHDF5_EXPORT_EXPLICIT
@@ -157,13 +169,21 @@ typedef HDF5TransformIOTemplate<double> HDF5TransformIO;
 //   We are using this library
 #    define ITKIOTransformHDF5_EXPORT_EXPLICIT ITKIOTransformHDF5_EXPORT
 #  endif
+namespace itk
+{
+
+#if defined( __GNUC__ )
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wattributes"
+#endif
+
 extern template class ITKIOTransformHDF5_EXPORT_EXPLICIT HDF5TransformIOTemplate< double >;
 extern template class ITKIOTransformHDF5_EXPORT_EXPLICIT HDF5TransformIOTemplate< float >;
-#  undef ITKIOTransformHDF5_EXPORT_EXPLICIT
+
+#if defined( __GNUC__ )
+#pragma GCC diagnostic pop
 #endif
 
 } // end namespace itk
-
-// Note: Explicit instantiation is done in itkHDF5TransformIO.cxx
-
-#endif // itkHDF5TransformIO_h
+#  undef ITKIOTransformHDF5_EXPORT_EXPLICIT
+#endif

Modules/IO/TransformInsightLegacy/include/itkTxtTransformIO.h
@@ -82,10 +82,22 @@ private:
 /** This helps to meet backward compatibility */
 typedef TxtTransformIOTemplate<double> TxtTransformIO;
 
+}
+
+// Note: Explicit instantiation is done in itkTxtTransformIO.cxx
+
+#endif // itkTxtTransformIO_h
+
 /** Explicit instantiations */
 #ifndef ITK_TEMPLATE_EXPLICIT_TxtTransformIO
 // Explicit instantiation is required to ensure correct dynamic_cast
 // behavior across shared libraries.
+//
+// IMPORTANT: Since within the same compilation unit,
+//            ITK_TEMPLATE_EXPLICIT_<classname> defined and undefined states
+//            need to be considered. This code *MUST* be *OUTSIDE* the header
+//            guards.
+//
 #  if defined( ITKIOTransformInsightLegacy_EXPORTS )
 //   We are building this library
 #    define ITKIOTransformInsightLegacy_EXPORT_EXPLICIT
@@ -93,13 +105,21 @@ typedef TxtTransformIOTemplate<double> TxtTransformIO;
 //   We are using this library
 #    define ITKIOTransformInsightLegacy_EXPORT_EXPLICIT ITKIOTransformInsightLegacy_EXPORT
 #  endif
-extern template class ITKIOTransformInsightLegacy_EXPORT_EXPLICIT TxtTransformIOTemplate< double >;
-extern template class ITKIOTransformInsightLegacy_EXPORT_EXPLICIT TxtTransformIOTemplate< float >;
-#  undef ITKIOTransformInsightLegacy_EXPORT_EXPLICIT
+namespace itk
+{
+
+#if defined( __GNUC__ )
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wattributes"
 #endif
 
-}
+extern template class ITKIOTransformInsightLegacy_EXPORT_EXPLICIT TxtTransformIOTemplate< double >;
+extern template class ITKIOTransformInsightLegacy_EXPORT_EXPLICIT TxtTransformIOTemplate< float >;
 
-// Note: Explicit instantiation is done in itkTxtTransformIO.cxx
+#if defined( __GNUC__ )
+#pragma GCC diagnostic pop
+#endif
 
-#endif // itkTxtTransformIO_h
+} // end namespace itk
+#  undef ITKIOTransformInsightLegacy_EXPORT_EXPLICIT
+#endif

Modules/IO/TransformMINC/include/itkMINCTransformIO.h
@@ -96,10 +96,22 @@ private:
 /** This helps to meet backward compatibility */
 typedef MINCTransformIOTemplate<double> MINCTransformIO;
 
+} // end namespace itk
+
+// Note: Explicit instantiation is done in itkMINCTransformIO.cxx
+
+#endif // itkMINCTransformIO_h
+
 /** Explicit instantiations */
 #ifndef ITK_TEMPLATE_EXPLICIT_MINCTransformIO
 // Explicit instantiation is required to ensure correct dynamic_cast
 // behavior across shared libraries.
+//
+// IMPORTANT: Since within the same compilation unit,
+//            ITK_TEMPLATE_EXPLICIT_<classname> defined and undefined states
+//            need to be considered. This code *MUST* be *OUTSIDE* the header
+//            guards.
+//
 #  if defined( ITKIOTransformMINC_EXPORTS )
 //   We are building this library
 #    define ITKIOTransformMINC_EXPORT_EXPLICIT
@@ -107,13 +119,21 @@ typedef MINCTransformIOTemplate<double> MINCTransformIO;
 //   We are using this library
 #    define ITKIOTransformMINC_EXPORT_EXPLICIT ITKIOTransformMINC_EXPORT
 #  endif
+namespace itk
+{
+
+#if defined( __GNUC__ )
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wattributes"
+#endif
+
 extern template class ITKIOTransformMINC_EXPORT_EXPLICIT MINCTransformIOTemplate< double >;
 extern template class ITKIOTransformMINC_EXPORT_EXPLICIT MINCTransformIOTemplate< float >;
-#  undef ITKIOTransformMINC_EXPORT_EXPLICIT
+
+#if defined( __GNUC__ )
+#pragma GCC diagnostic pop
 #endif
 
 } // end namespace itk
-
-// Note: Explicit instantiation is done in itkMINCTransformIO.cxx
-
-#endif // itkMINCTransformIO_h
+#  undef ITKIOTransformMINC_EXPORT_EXPLICIT
+#endif

Modules/IO/TransformMatlab/include/itkMatlabTransformIO.h
@@ -71,10 +71,22 @@ protected:
 /** This helps to meet backward compatibility */
 typedef MatlabTransformIOTemplate<double> MatlabTransformIO;
 
+}
+
+// Note: Explicit instantiation is done in itkMatlabTransformIO.cxx
+
+#endif // itkMatlabTransformIO_h
+
 /** Explicit instantiations */
 #ifndef ITK_TEMPLATE_EXPLICIT_MatlabTransformIO
 // Explicit instantiation is required to ensure correct dynamic_cast
 // behavior across shared libraries.
+//
+// IMPORTANT: Since within the same compilation unit,
+//            ITK_TEMPLATE_EXPLICIT_<classname> defined and undefined states
+//            need to be considered. This code *MUST* be *OUTSIDE* the header
+//            guards.
+//
 #  if defined( ITKIOTransformMatlab_EXPORTS )
 //   We are building this library
 #    define ITKIOTransformMatlab_EXPORT_EXPLICIT
@@ -82,13 +94,21 @@ typedef MatlabTransformIOTemplate<double> MatlabTransformIO;
 //   We are using this library
 #    define ITKIOTransformMatlab_EXPORT_EXPLICIT ITKIOTransformMatlab_EXPORT
 #  endif
-extern template class ITKIOTransformMatlab_EXPORT_EXPLICIT MatlabTransformIOTemplate< double >;
-extern template class ITKIOTransformMatlab_EXPORT_EXPLICIT MatlabTransformIOTemplate< float >;
-#  undef ITKIOTransformMatlab_EXPORT_EXPLICIT
+namespace itk
+{
+
+#if defined( __GNUC__ )
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wattributes"
 #endif
 
-}
+extern template class ITKIOTransformMatlab_EXPORT_EXPLICIT MatlabTransformIOTemplate< double >;
+extern template class ITKIOTransformMatlab_EXPORT_EXPLICIT MatlabTransformIOTemplate< float >;
 
-// Note: Explicit instantiation is done in itkMatlabTransformIO.cxx
+#if defined( __GNUC__ )
+#pragma GCC diagnostic pop
+#endif
 
-#endif // itkMatlabTransformIO_h
+} // end namespace itk
+#  undef ITKIOTransformMatlab_EXPORT_EXPLICIT
+#endif

Modules/Remote/IOTransformDCMTK.remote.cmake
@@ -3,6 +3,6 @@ itk_fetch_module(IOTransformDCMTK
   "The modules contains classes for reading spatial transform from DICOM
   files. See http://www.insight-journal.org/browse/publication/923
              http://hdl.handle.net/10380/3468"
-  GIT_REPOSITORY ${git_protocol}://github.com/InsightSoftwareConsortium/ITKIOTransformDCMTK.git
-  GIT_TAG c2985f9ff5a75128c26ffc0beac800c617aa0b07
+  GIT_REPOSITORY ${git_protocol}://github.com/jcfr/ITKIOTransformDCMTK.git
+  GIT_TAG c03525c0d852310244f6022261f9b80e83ed7832
   )