18e003085057872b400bf581a2ab693142a37427
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2015-12-18 12:26:24 -0500

    STYLE: Refactor code introducing ITK_GCC_PRAGMA_DIAG_(OFF|ON) macros.
    See #3393
    
    Change-Id: I4cc6bf16471706685ff14c38563aa55b3a7ab4b4

--- Modules/IO/TransformBase/include/itkTransformFileWriter.h
+++ Modules/IO/TransformBase/include/itkTransformFileWriter.h
@@ -111,14 +111,7 @@ private:
 /** This helps to meet backward compatibility */
 typedef itk::TransformFileWriterTemplate<double> TransformFileWriter;
 
-#if defined( __GNUC__ ) && !defined( __INTEL_COMPILER )
-#  if ( __GNUC__ > 4 ) || (( __GNUC__ >= 4 ) && ( __GNUC_MINOR__ >= 2 ))
-#    if ( __GNUC_MINOR__ >= 6 )
-#      pragma GCC diagnostic push
-#    endif
-#    pragma GCC diagnostic ignored "-Wattributes"
-#  endif
-#endif
+ITK_GCC_PRAGMA_DIAG_OFF("-Wattributes")
 
 /** Declare specializations */
 template<> void ITKIOTransformBase_EXPORT TransformFileWriterTemplate< double >::PushBackTransformList(const Object *transObj);
@@ -131,15 +124,7 @@ extern template class ITKIOTransformBase_EXPORT TransformFileWriterTemplate< dou
 extern template class ITKIOTransformBase_EXPORT TransformFileWriterTemplate< float >;
 #endif
 
-#if defined( __GNUC__ ) && !defined( __INTEL_COMPILER )
-#  if ( __GNUC__ > 4 ) || (( __GNUC__ >= 4 ) && ( __GNUC_MINOR__ >= 2 ))
-#    if ( __GNUC_MINOR__ >= 6 )
-#      pragma GCC diagnostic pop
-#    else
-#      pragma GCC diagnostic warning "-Wattributes"
-#    endif
-#  endif
-#endif
+ITK_GCC_PRAGMA_DIAG_ON("-Wattributes")
 
 } // namespace itk
 
