71e38773eb1e709d90d7978acdfd1c91f11b791d
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2015-12-18 18:29:43 -0500

    COMP: TransformIO: Handle compiler not supporting "pragma diagnostic".
    *** DO NOT INTEGRATE: IOTransformDCMTK PR#5 needs to be integrated first.
    
    This commit fixes warnings introduced in eb2316a (BUG: TransformIO:
    Implement explicit template instantiation using "extern".) by handling
    version of GCC not supporting "pragma diagnostic" preprocessor instruction.
    
    It does so by introducing two new macros based on comments from Patrick Horgan
    posted on the gcc mailing list. See https://gcc.gnu.org/ml/gcc-help/2011-01/msg00133.html
    
      ITK_GCC_PRAGMA_DIAG_PUSH()
      ITK_GCC_PRAGMA_DIAG_POP()
      ITK_GCC_PRAGMA_DIAG(param1 [param2 [...]])
    
    It also introduces macro ITK_HAS_GCC_PRAGMA_DIAG_PUSHPOP avoiding to
    systematically check if Intel compiler is used and if the version of GCC
    is the correct one. Later one, this macro could even be set using a
    try compile.
    
    Considering that
    
      (1) the "pragma diagnostic" support has been first introduced
      in GCC  4.2 (r109907 / gcc-mirror/gcc@76f0251)
    
    and
    
      (2) the support for push/pop has only been introduced later in
      GCC 4.6 (r161115 / gcc-mirror/gcc@0955be6)
    
    this commit updates the code so that:
    
      (a) no pragma is added for both GCC < 4.2 and Intel compiler.
    
      (b) simple set/unset of "-Wattributes" is done for GCC >= 4.2 and  < 4.6.
          This will systematically resets the diagnostic level to "warning".
          The caveat of this approach is that is the user build ITK with
          -Werror flag, following "-Wattributes" warning (if any) won't be
          considered as error.
    
      (c) previous diagnostic context is restored on GCC >= 4.6 by using
          push/pop functionality.
    
    Warnings fixes by this commit are:
    
      on GCC 4.1: ignoring #pragma GCC diagnostic
    
      on ICC 1.3: unrecognized #pragma
    
      on GCC 4.4: expected [error|warning|ignored] after '#pragma GCC diagnostic'
    
    Notes:
    
      * it also updates IOTransformDCMTK remote module applying a
        similar fix.
    
      * Since Intel compiler defined and based on the GCC compiler "found"
        at compilation time [1], this commit explicitly excludes the intel
        compiler.
    
      * Introduce convenient macro "ITK_PRAGMA"
    
    [1] https://software.intel.com/sites/default/files/article/328248/intel-linux-compiler-13-0-gnu-compatibility-rev6.pdf
    
    Change-Id: Ib1cb52e84dede5829169e658ce23c4da49508b2d

--- Modules/Remote/IOTransformDCMTK.remote.cmake
+++ Modules/Remote/IOTransformDCMTK.remote.cmake
@@ -3,6 +3,6 @@ itk_fetch_module(IOTransformDCMTK
   "The modules contains classes for reading spatial transform from DICOM
   files. See http://www.insight-journal.org/browse/publication/923
              http://hdl.handle.net/10380/3468"
-  GIT_REPOSITORY ${git_protocol}://github.com/InsightSoftwareConsortium/ITKIOTransformDCMTK.git
-  GIT_TAG c2985f9ff5a75128c26ffc0beac800c617aa0b07
+  GIT_REPOSITORY ${git_protocol}://github.com/jcfr/ITKIOTransformDCMTK.git
+  GIT_TAG 97dfe3336b4e1cf38493fb9d1688e272deb73599
   )
